f55e489135cae999d64f5cf68331afa019e55ba6
cyber-ivern <avery.kwok05@yahoo.com>
2025-03-18 22:11:28 -0400

    update to 3 second rule

--- api/utils/commands.js
+++ api/utils/commands.js
@@ -1,5 +1,4 @@
 const { InteractionResponseType } = require('discord-interactions');
-const { deferReply } = require(`discord.js`);
 
 const commands = {
   hello: (message) => ({
@@ -20,11 +19,7 @@ const commands = {
 
       console.log(`checkpoint 1`);
       // First, acknowledge the command immediately
-      await fetch(`https://discord.com/api/v10/interactions/${message.id}/${message.token}/callback`, {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ type: 5 }) // DEFERRED_CHANNEL_MESSAGE_WITH_SOURCE
-    });
+      await message.deferReply();
     
     console.log(`checkpoint 2`);
 
